5ccb6cee8cf7b84d7e5e1c4e204345897ce0c004
Huang Jian <435360@qq.com>
2016-04-09 13:33:49 +0800

    1.检查Welcome group下vc dealloc 2.增加登录／注册选择页面背景动画

--- ShuchuangClient/WelcomeViewController.m
+++ ShuchuangClient/WelcomeViewController.m
@@ -9,10 +9,12 @@
 #import "WelcomeViewController.h"
 #import "UIButton+FillBackgroundImage.h"
 #import "Bmob.h"
+#import "MyWelcomeView.h"
 @interface WelcomeViewController ()
 @property (weak, nonatomic) IBOutlet UIButton *btnReg;
 @property (weak, nonatomic) IBOutlet UIButton *btnLogin;
-@property (weak, nonatomic) IBOutlet UIImageView *backgroundView;
+@property (strong, nonatomic) MyWelcomeView *bgView;
+
 
 - (IBAction)onBtnReg:(id)sender;
 - (IBAction)onBtnLogin:(id)sender;
@@ -24,10 +26,10 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    [self.btnReg setBackgroundImage:[UIButton imageWithColor:[UIButton getColorFromHex:0xffd573 Alpha:0.4]] forState:UIControlStateNormal];
-    [self.btnLogin setBackgroundImage:[UIButton imageWithColor:[UIButton getColorFromHex:0xffba73 Alpha:0.4]] forState:UIControlStateNormal];
-    [self.btnReg setBackgroundImage:[UIButton imageWithColor:[UIButton getColorFromHex:0xffb300 Alpha:0.4]] forState:UIControlStateHighlighted];
-    [self.btnLogin setBackgroundImage:[UIButton imageWithColor:[UIButton getColorFromHex:0xff8100 Alpha:0.4]] forState:UIControlStateHighlighted];
+    [self.btnReg setBackgroundImage:[UIButton imageWithColor:[UIButton getColorFromHex:0xe5e5e5 Alpha:0.4]] forState:UIControlStateNormal];
+    [self.btnLogin setBackgroundImage:[UIButton imageWithColor:[UIButton getColorFromHex:0xe5e5e5 Alpha:0.4]] forState:UIControlStateNormal];
+    [self.btnReg setBackgroundImage:[UIButton imageWithColor:[UIButton getColorFromHex:0xa3a3a3 Alpha:0.4]] forState:UIControlStateHighlighted];
+    [self.btnLogin setBackgroundImage:[UIButton imageWithColor:[UIButton getColorFromHex:0xa3a3a3 Alpha:0.4]] forState:UIControlStateHighlighted];
     [self.btnReg.layer setBorderWidth:1.0];
     [self.btnReg.layer setBorderColor:[[UIColor colorWithWhite:1.0 alpha:0.9] CGColor]];
     [self.btnLogin.layer setBorderWidth:1.0];
@@ -38,13 +40,26 @@
     self.btnLogin.layer.cornerRadius = 5.0;
     self.btnLogin.layer.opaque = NO;
     self.btnLogin.layer.masksToBounds = YES;
+    self.bgView = [[MyWelcomeView alloc] initWithFrameInView:self.view];
+    [self.view addSubview:self.bgView];
     
+}
+
+- (void)dealloc {
+    [self.bgView stopAnimation];
+    self.bgView = nil;
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (void)viewDidAppear:(BOOL)animated {
+    [self.bgView startAnimation];
+    [super viewDidAppear:animated];
+}
+
 
 #pragma mark - Navigation
 
